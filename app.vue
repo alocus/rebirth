160211bc9fb5acb9141d1b85768a505c87a88e9b
Michael Dunn <michaeldunn@Michaels-MacBook-Pro.local>
2022-12-24 17:49:48 -0500

    initial commit

---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+@import '@vue-flow/core/dist/style.css';
+@import '@vue-flow/core/dist/theme-default.css';
+
+html,
+body,
+#__nuxt {
+  margin: 0;
+  height: 100%;
+}
+
+#__nuxt {
+  text-transform: uppercase;
+  font-family: 'JetBrains Mono', monospace;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ components/Pinia.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { VueFlow, useVueFlow } from '@braks/vue-flow';
+import { computed } from 'vue';
+// import useStore from './stores/ElementsStore.ts';
+
+const store = useElementStore();
+
+const { onConnect, addEdges } = useVueFlow();
+
+onConnect((params) => addEdges([params]));
+</script>
+
+<template>
+  <VueFlow v-model="store.elements" :fit-view-on-init="true">
+    <div style="position: absolute; right: 10px; top: 10px; z-index: 4">
+      <button style="margin-right: 5px" @click="store.updatePosition">
+        update positions
+      </button>
+      <button style="margin-right: 5px" @click="store.toggleClass">
+        toggle class
+      </button>
+      <button @click="store.log">log store state</button>
+    </div>
+  </VueFlow>
+</template>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
