79d781fb469947c7bf3ae876c00306961323d18a
Michael Dunn <mdunn@alocus.com>
2022-12-30 17:17:40 -0500

    Update toolbar and icon

--- components/Editor.vue
+++ components/Editor.vue
@@ -33,12 +33,15 @@ const { setNodes, setEdges, dimensions, onPaneReady, onNodeDragStop, onConnect, 
 /**
  * Our elements
  */
- const elements = ref(initialElements)
+const elements = ref(initialElements)
 
- const onSave = () => {
+const onSave = () => {
   localStorage.setItem(flowKey, JSON.stringify(toObject()))
 }
 
+/**
+ * Restore graph from localStorage - like an Undo before save
+ */
 const onRestore = () => {
   const flow = JSON.parse(localStorage.getItem(flowKey))
 
@@ -77,13 +80,14 @@ const onDragOver = (event) => {
 onConnect((params) => addEdges([params]))
 
 const dark = ref(false)
+const showFileSelector = ref(false)
 
 
 /**
  * To update node properties you can simply use your elements v-model and mutate the elements directly
  * Changes should always be reflected on the graph reactively, without the need to overwrite the elements
  */
- const updatePos = () =>
+const updatePos = () =>
   elements.value.forEach((el) => {
     if (isNode(el)) {
       el.position = {
@@ -103,22 +107,35 @@ const logToObject = () => console.log(toObject())
  */
 const resetTransform = () => setTransform({ x: 0, y: 0, zoom: 1 })
 
+/**
+ * Toggle dark and light theme
+ */
 const toggleClass = () => (dark.value = !dark.value)
 
-const saveFile = () => {
+/**
+ * show import file selector
+ */
+const toggleFileSelector = () => (showFileSelector.value = !showFileSelector.value)
+
+// Save Graph to node-flow.json
+const exportFile = () => {
   const data = JSON.stringify(toObject())
-	let filename = 'node-flow.json';
-	let element = document.createElement('a');
-	element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(data));
-	element.setAttribute('download', filename);
+  let filename = 'node-flow.json';
+  let element = document.createElement('a');
+  element.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(data));
+  element.setAttribute('download', filename);
 
-	element.style.display = 'none';
-	document.body.appendChild(element);
+  element.style.display = 'none';
+  document.body.appendChild(element);
 
-	element.click();
-	document.body.removeChild(element); 
+  element.click();
+  document.body.removeChild(element);
 }
 
+/**
+ * Drop action of a node pulling from sidebar
+ * @param {} event 
+ */
 const onDrop = (event) => {
   const type = event.dataTransfer?.getData('application/vueflow')
 
@@ -157,91 +174,32 @@ const onDrop = (event) => {
 
 <template>
   <div class="dndflow" @drop="onDrop">
-    <VueFlow @dragover="onDragOver" v-model="elements" :class="{ dark }" class="basicflow" :default-zoom="1.5" :min-zoom="0.2" :max-zoom="4">
+    <VueFlow @dragover="onDragOver" v-model="elements" :class="{ dark }" class="basicflow" :default-zoom="1.5"
+      :min-zoom="0.2" :max-zoom="4">
+
       <Background :pattern-color="dark ? '#FFFFFB' : '#aaa'" gap="8" />
       <MiniMap />
       <Controls />
-
-
       <Panel :position="PanelPosition.TopRight" class="controls">
-        <button style="background-color: #113285; color: white" title="Reset Transform" @click="resetTransform">
-          <svg width="16" height="16" viewBox="0 0 32 32">
-            <path fill="#FFFFFB" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z" />
-          </svg>
-        </button>
-
-        <button style="background-color: #113285; color: white" title="Reset Transform" @click="onRestore">
-          <svg width="16" height="16" viewBox="0 0 32 32">
-            <path fill="#FFFFFB" d="M18 28A12 12 0 1 0 6 16v6.2l-3.6-3.6L1 20l6 6l6-6l-1.4-1.4L8 22.2V16a10 10 0 1 1 10 10Z" />
-          </svg>
-        </button>
-
-        <button style="background-color: #6f3381" title="Shuffle Node Positions" @click="onSave">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-floppy" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="#FFFFFB"/>
-            <path d="M6 4h10l4 4v10a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2v-12a2 2 0 0 1 2 -2" />
-            <circle cx="12" cy="14" r="2" />
-            <polyline points="14 4 14 8 8 8 8 4" />
-          </svg>
-        </button>
-
-        <button
-          :style="{ backgroundColor: dark ? '#FFFFFB' : '#292524', color: dark ? '#292524' : '#FFFFFB' }"
-          @click="toggleClass"
-        >
-          <template v-if="dark">
-            <svg width="16" height="16" viewBox="0 0 24 24">
-              <path
-                fill="#292524"
-                d="M12 17q-2.075 0-3.537-1.463Q7 14.075 7 12t1.463-3.538Q9.925 7 12 7t3.538 1.462Q17 9.925 17 12q0 2.075-1.462 3.537Q14.075 17 12 17ZM2 13q-.425 0-.712-.288Q1 12.425 1 12t.288-.713Q1.575 11 2 11h2q.425 0 .713.287Q5 11.575 5 12t-.287.712Q4.425 13 4 13Zm18 0q-.425 0-.712-.288Q19 12.425 19 12t.288-.713Q19.575 11 20 11h2q.425 0 .712.287q.288.288.288.713t-.288.712Q22.425 13 22 13Zm-8-8q-.425 0-.712-.288Q11 4.425 11 4V2q0-.425.288-.713Q11.575 1 12 1t.713.287Q13 1.575 13 2v2q0 .425-.287.712Q12.425 5 12 5Zm0 18q-.425 0-.712-.288Q11 22.425 11 22v-2q0-.425.288-.712Q11.575 19 12 19t.713.288Q13 19.575 13 20v2q0 .425-.287.712Q12.425 23 12 23ZM5.65 7.05L4.575 6q-.3-.275-.288-.7q.013-.425.288-.725q.3-.3.725-.3t.7.3L7.05 5.65q.275.3.275.7q0 .4-.275.7q-.275.3-.687.287q-.413-.012-.713-.287ZM18 19.425l-1.05-1.075q-.275-.3-.275-.712q0-.413.275-.688q.275-.3.688-.287q.412.012.712.287L19.425 18q.3.275.288.7q-.013.425-.288.725q-.3.3-.725.3t-.7-.3ZM16.95 7.05q-.3-.275-.287-.688q.012-.412.287-.712L18 4.575q.275-.3.7-.288q.425.013.725.288q.3.3.3.725t-.3.7L18.35 7.05q-.3.275-.7.275q-.4 0-.7-.275ZM4.575 19.425q-.3-.3-.3-.725t.3-.7l1.075-1.05q.3-.275.713-.275q.412 0 .687.275q.3.275.288.688q-.013.412-.288.712L6 19.425q-.275.3-.7.287q-.425-.012-.725-.287Z"
-              />
-            </svg>
-          </template>
-
-          <template v-else>
-            <svg width="16" height="16" viewBox="0 0 24 24">
-              <path
-                fill="#FFFFFB"
-                d="M12 21q-3.75 0-6.375-2.625T3 12q0-3.75 2.625-6.375T12 3q.35 0 .688.025q.337.025.662.075q-1.025.725-1.637 1.887Q11.1 6.15 11.1 7.5q0 2.25 1.575 3.825Q14.25 12.9 16.5 12.9q1.375 0 2.525-.613q1.15-.612 1.875-1.637q.05.325.075.662Q21 11.65 21 12q0 3.75-2.625 6.375T12 21Z"
-              />
-            </svg>
-          </template>
-        </button>
-
-        <button title="Log `toObject`" @click="logToObject">
-          <svg width="16" height="16" viewBox="0 0 24 24">
-            <path
-              fill="#292524"
-              d="M20 19V7H4v12h16m0-16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16m-7 14v-2h5v2h-5m-3.42-4L5.57 9H8.4l3.3 3.3c.39.39.39 1.03 0 1.42L8.42 17H5.59l3.99-4Z"
-            />
-          </svg>
-        </button>
-        <VFileInput
-          label="Plain"
-          variant="plain"
-        />
-        <button title="Import" @click="importFile">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-upload" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-            <polyline points="7 9 12 4 17 9" />
-            <line x1="12" y1="4" x2="12" y2="16" />
-          </svg>
-        </button>
-
-        <button title="Export" @click="saveFile">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-download" width="16" height="16" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />
-            <polyline points="7 11 12 16 17 11" />
-            <line x1="12" y1="4" x2="12" y2="16" />
-          </svg>
-        </button>
+        <v-btn title="Import" @click="toggleFileSelector" icon="mdi-file-import"> </v-btn>
+        <v-btn title="Export" @click="exportFile" icon="mdi-file-export"> </v-btn>
+        <v-btn title="Reset" @click="resetTransform" icon="mdi-crop-portrait"></v-btn>
+        <v-btn title="Load" @click="onRestore" icon="mdi-restore"></v-btn>
+        <v-btn title="Save" color="secondary" @click="onSave" icon="mdi-content-save"></v-btn>
+        <v-btn title="Dark" color="primary" @click="toggleClass" icon="mdi-theme-light-dark"></v-btn>
+        <!-- 
+          <v-btn :style="{ backgroundColor: dark ? '#FFFFFB' : '#292524', color: dark ? '#292524' : '#FFFFFB' }"
+          @click="toggleClass"  :prepend-icon="dark ? 'mdi-white-balance-sunny' : 'mdi-moon-waning-crescent' " >
+          </v-btn>
+        -->
+        <v-btn title="LogToObject" @click="logToObject" icon="mdi-export" ></v-btn>
+        <VFileInput label="Graph JSONs" chips multiple v-if="showFileSelector" accept=".json" icon="mdi-file-import"></VFileInput>
       </Panel>
     </VueFlow>
     <Sidebar />
   </div>
 </template>
+<style>
 @import 'https://cdn.jsdelivr.net/npm/@vue-flow/core@1.9.1/dist/style.css';
 @import 'https://cdn.jsdelivr.net/npm/@vue-flow/core@1.9.1/dist/theme-default.css';
 @import 'https://cdn.jsdelivr.net/npm/@vue-flow/controls@latest/dist/style.css';
@@ -269,4 +227,15 @@ body,
   transform-origin: bottom right;
 }
 
-.basicflow.dark{background:#57534e;color:#fffffb}.basicflow.dark .vue-flow__node{background:#292524;color:#fffffb}.basicflow.dark .vue-flow__controls .vue-flow__controls-button{background:#292524;fill:#fffffb;border-color:#fffffb}.basicflow.dark .vue-flow__edge-textbg{fill:#292524}.basicflow.dark .vue-flow__edge-text{fill:#fffffb}.basicflow .controls{display:flex;flex-wrap:wrap;justify-content:center;gap:8px}.basicflow .controls button{padding:4px;border-radius:5px;font-weight:600;-webkit-box-shadow:0px 5px 10px 0px rgba(0,0,0,.3);box-shadow:0 5px 10px #0000004d;cursor:pointer;display:flex;justify-content:center;align-items:center}.basicflow .controls button:hover{transform:scale(102%);transition:.25s all ease}
+.basicflow.dark{background:#57534e;color:#fffffb}.basicflow.dark .vue-flow__node{background:#292524;color:#fffffb}.basicflow.dark .vue-flow__controls .vue-flow__controls-button{background:#292524;fill:#fffffb;border-color:#fffffb}.basicflow.dark .vue-flow__edge-textbg{fill:#292524}.basicflow.dark .vue-flow__edge-text{fill:#fffffb}
+.mbasicflow .tmp .control{display:flex;flex-wrap:wrap;justify-content:center;gap:8px}
+.mbasicflow .tmp .control button{padding:4px;border-radius:5px;font-weight:600;-webkit-box-shadow:0px 5px 10px 0px rgba(0,0,0,.3);box-shadow:0 5px 10px #0000004d;cursor:pointer;display:flex;justify-content:center;align-items:center}.basicflow .controls button:hover{transform:scale(102%);transition:.25s all ease}
+
+.mbasicflow .controls button{
+  width:24px;
+  height:24px;
+  background-color: #113285; 
+  color: #FFFFFB
+}
+
+</style>
