94b55031a5a441d1e5deff0d7a64ecb22c9493bc
Michael Dunn <mdunn@alocus.com>
2022-12-31 15:43:20 -0500

    Add debug config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  sourcemap: {
+    server: true,
+    client: true
+  },
+  typescript: {
+    shim: false
+  },
+  
   // https://github.com/CodyBontecou/nuxt3-and-vuetify/blob/main/nuxt.config.ts
   css: ["vuetify/lib/styles/main.sass", "@mdi/font/css/materialdesignicons.min.css"],
   build: {
     transpile: ['vuetify'],
+    
   },
   vite: {
+    build: {
+      sourcemap: 'inline'
+    },
     define: {
       'process.env.DEBUG': false,
     },
